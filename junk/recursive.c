57835b6bee126c6cf31595b98c29c207d92cfb96
Eray Guler <21058071@std.yildiz.edu.tr>
2023-03-02 13:07:04 +0300

    recursive.c

--- junk/recursive.c
+++ junk/recursive.c
@@ -1,1 +1,16 @@
 #include<stdio.h>
+
+void f(int a);
+
+int main(int argc, char const *argv[])
+{
+    //f(10);
+    return 0;
+}
+
+/*void f(int a){//araliktaki sayilari yazdirma
+    if(a<0) return;
+    printf("%d\n",a);
+    f(a-1);
+    return;
+}*/
